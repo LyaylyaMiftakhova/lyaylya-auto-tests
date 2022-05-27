6bbc20a5430e50e48a8a6297cac3b7c813e14e6e
miftakhovalyaylya <lyaylyamiftakhova@gmail.com>
2022-05-27 20:57:11 +0300

    Add test

---
+++ src/test/java/AuthTest.java
@@ -0,0 +1,21 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AuthTest {
+
+    @Test
+    public void shouldAuthorizeTest() {
+        open("https://github.com/");
+        $("[href='/login']").click();
+        $("[id='login_field']").sendKeys("LyaylyaMiftakhova");
+        $("[id='password']").sendKeys("2206marsL");
+        $(".js-sign-in-button").click();
+        $(".Header").shouldBe(Condition.visible);
+        $("[aria-label='View profile and more']").click();
+        $(byText("Your profile")).click();
+        $(byText("training-repository")).shouldBe(Condition.visible);
+    }
+}
