1c045a9beca081ee0088404073651d216140c244
miftakhovalyaylya <lyaylyamiftakhova@gmail.com>
2022-06-04 21:08:56 +0300

    Rewrote test according to Page Objects pattern

--- src/test/java/AuthTest.java
+++ src/test/java/AuthTest.java
@@ -1,4 +1,6 @@
 import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selectors.byText;
@@ -6,16 +8,29 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class AuthTest {
 
-    @Test
-    public void shouldAuthorizeTest() {
+    @BeforeEach
+    public void setup(){
         open("https://github.com/");
-        $("[href='/login']").click();
-        $("[id='login_field']").sendKeys("LyaylyaMiftakhova");
-        $("[id='password']").sendKeys("2206marsL");
-        $(".js-sign-in-button").click();
-        $(".Header").shouldBe(Condition.visible);
-        $("[aria-label='View profile and more']").click();
-        $(byText("Your profile")).click();
-        $(byText("training-repository")).shouldBe(Condition.visible);
+        TestPages.mainPage.mainSingInButton()
+                .click();
+    }
+
+    @Test
+    @DisplayName("Авторизация и переход в профиль")
+    public void shouldAuthorizeTest() {
+        TestPages.mainPage.loginInput()
+                .sendKeys("LyaylyaMiftakhova");
+        TestPages.mainPage.passwordInput()
+                        .sendKeys("11111");
+        TestPages.mainPage.singInButton()
+                        .click();
+        TestPages.mainPage.header()
+                        .shouldBe(Condition.visible);
+        TestPages.mainPage.dropdownButton()
+                        .click();
+        $(byText("Your profile"))
+                        .click();
+        $(byText("training-repository"))
+                        .shouldBe(Condition.visible);
     }
 }
