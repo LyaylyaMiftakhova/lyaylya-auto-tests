a5f996648b766f9fc095ef8df45f44dff0309f0f
miftakhovalyaylya <lyaylyamiftakhova@gmail.com>
2022-06-06 22:59:08 +0300

    Connect allure and add steps

--- src/test/java/JunitTest.java
+++ src/test/java/JunitTest.java
@@ -1,3 +1,6 @@
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,8 +13,11 @@ import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
+@Owner("LyaylyaMiftakhova")
+@Feature("JUnit")
 public class JunitTest {
 
     @BeforeEach
@@ -24,27 +30,40 @@ public class JunitTest {
     @Test
     @DisplayName("Переключение на ветку fixtures")
     public void shouldSwitchToBranchTest(){
-        TestPages.junitPage.dropdownBranches()
-                .click();
-        TestPages.junitPage.fixturesBranches()
-                .click();
+        step("Кликнуть по кнопке выпадающего списка", () ->
+                TestPages.junitPage.dropdownBranches()
+                        .click());
+        step("Перейти в ветку 'fixtures'", () ->
+                TestPages.junitPage.fixturesBranches()
+                        .click());
+        step("Проверить, что в наименовании ветки присутствует текст 'fixtures'", () -> {
         TestPages.junitPage.nameFixturesBranches()
                 .shouldHave(text("fixtures"));
+        });
     }
 
+    @Story("Поиск")
     @DisplayName("Позитивные проверки поиска")
     @MethodSource("positiveChecks")
     @ParameterizedTest(name = "{displayName} {0}")
     public void positiveSearchChecksTest(String type, String searchData, String releaseName){
-        TestPages.junitPage.releasesLink()
+        step("Перейти в релизы репозитория 'junit4'", () -> {
+            TestPages.junitPage.releasesLink()
                 .click();
-        TestPages.junitPage.nameReleasesSwitch()
+        });
+        step("Проверить, что в переключателе присуствует 'Releases'", () -> {
+            TestPages.junitPage.nameReleasesSwitch()
                 .shouldHave(text("Releases"));
+        });
 
-        TestPages.junitPage.searchInput()
+        step("В поле поиска ввести данные для поиска и нажать ENTER", () -> {
+            TestPages.junitPage.searchInput()
                 .sendKeys(searchData + Keys.ENTER);
-        TestPages.junitPage.selectorCart()
+        });
+        step("Проверить, что отображается искомая версия релиза", () -> {
+            TestPages.junitPage.selectorCart()
                 .shouldHave(text(releaseName));
+        });
     }
 
     static Stream<Arguments> positiveChecks() {
